c738b7c0c54595f4d73f3acf87d37ef35b30a8ce
Bande <32582441+abdulsalam-bande@users.noreply.github.com>
2021-08-03 01:54:45 +0300

    Addded print

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import pandas as pd
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_absolute_error
+priint('aksdma')
 
 
 def calculate_metrics(predictions, target):
