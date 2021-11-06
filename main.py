4824d170d263dd7302986d8276b7345792fe775e
Bande <32582441+abdulsalam-bande@users.noreply.github.com>
2021-11-06 22:52:02 +0300

    Added Command Line Arguments

--- main.py
+++ main.py
@@ -4,15 +4,36 @@ import pandas as pd
 from testing import test_models
 from utils import get_training_and_test_data, save_dict
 from trainer import trainer
+import argparse
+import warnings
+warnings.filterwarnings("ignore")
 
-targets = ['target1']
-info = {'mac': [167, 'mac_keys_fingerprints(smile)']}
-train_size = [50]
+
+parser = argparse.ArgumentParser(description="train code for training a network to estimate depth ", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--targets", type=str, help="specify the target protein to ", nargs='+')
+parser.add_argument("--descriptors", type=str, help="specify the training descriptor", nargs='+')
+parser.add_argument("--training_size", type=int, help="number of epochs", nargs='+')
+args = parser.parse_args()
+
+
+targets = args.targets
+info = {'mac': [167, 'mac_keys_fingerprints(smile)'],
+        'onehot': [3500, 'one_hot_encode(smile)'],
+        'morgan_onehot_mac': [4691, 'morgan_fingerprints_mac_and_one_hot(smile)'],
+        'morgan_onehot_mac_circular': [4755, 'morgan_fingerprints_mac_and_one_hot_descriptors_CircularFingerprint(smile)']}
+
+info2 = {}
+for desc in args.descriptors:
+    if desc in info:
+        info2[desc] = [info[desc][0], info[desc][1]]
+
+print('info2', info2)
+
+train_size = args.training_size
 training_metrics_dir = 'Results/training_metrics/'
 testing_metrics_dir = 'Results/testing_metrics/'
 test_predictions_dir = 'Results/test_predictions/'
 project_info_dir = 'Results/project_info/'
-
 dataset_dir = "Datasets/"
 os.makedirs(training_metrics_dir, exist_ok=True)
 os.makedirs(testing_metrics_dir, exist_ok=True)
@@ -20,19 +41,18 @@ os.makedirs(test_predictions_dir, exist_ok=True)
 os.makedirs(project_info_dir, exist_ok=True)
 
 for target in targets:
-    for key, value in info.items():
+    for key, value in info2.items():
         for size in train_size:
             identifier = f"{target}_{key}_{str(size)}"
             print('identifier ', identifier)
             numb_of_features = value[0]
             descriptor = value[1]
-            PATH_TO_CSV = "/Users/abdulsalamyazid/Desktop/thesis/Projects/Local/Swift Dock/Data Set/Target 2/target_2_clean.csv"
-            # PATH_TO_CSV = f"{dataset_dir}/{target}"
+            PATH_TO_CSV = f"{dataset_dir}/{target}.csv"
             DATA = pd.read_csv(PATH_TO_CSV)
             data_set_size = len(DATA)
             TRAINING_SIZE = size
-            number_of_folds = 2
-            TESTING_SIZE = 5
+            number_of_folds = 5
+            TESTING_SIZE = 30
             TRAINING_SIZE = TRAINING_SIZE * number_of_folds
             feature_dim = numb_of_features
             train, test = get_training_and_test_data(DATA, TRAINING_SIZE, TESTING_SIZE)
